db44334798569de66aecc5dcbdae83513ee61a2a
rbokel <rbokel.github@qlod.org>
2016-09-29 11:17:23 +0200

    Add outcome method. Same as model it delegates to the component controller

--- test/index-test.js
+++ test/index-test.js
@@ -5,22 +5,30 @@ import sinon from 'sinon';
 
 import Controller from '../lib/index';
 
-describe('model', () => {
+describe('pie-client-side-controller', () => {
 
-  let model, controllerMap, controller, myPieController, modelResults;
+  let model, controllerMap, controller, myPieController;
 
   beforeEach((done) => {
-    model = [
-      {
-        id: '1', component: {
-          name: 'my-pie'
-        }
+    model = [{
+      id: '1',
+      component: {
+        name: 'my-pie'
       }
-    ];
+      }];
 
     myPieController = {
+      outcome: sinon.spy((model) => {
+        return Promise.resolve({
+          id: model.id,
+          value: 'outcome-response'
+        });
+      }),
       model: sinon.spy((model) => {
-        return Promise.resolve({ id: model.id, value: 'model-response' });
+        return Promise.resolve({
+          id: model.id,
+          value: 'model-response'
+        });
       })
     }
 
@@ -29,20 +37,76 @@ describe('model', () => {
     }
 
     controller = new Controller(model, controllerMap);
-
-    controller.model(['1'], [{ id: '1', value: 'session' }], { mode: 'gather' })
-      .then((results) => {
-        modelResults = results;
-        done();
-      })
-      .catch(done);
+    done();
   });
 
-  it('should delegate calls to the underlying controllers', () => {
-    sinon.assert.calledWith(myPieController.model, model[0], { id: '1', value: 'session' }, { mode: 'gather' });
+  describe('outcome', () => {
+
+    let outcomeResults;
+
+    beforeEach((done) => {
+      controller.outcome(['1'], [{
+          id: '1',
+          value: 'session'
+        }], {
+          mode: 'gather'
+        })
+        .then((results) => {
+          outcomeResults = results;
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should delegate calls to the underlying controllers', () => {
+      sinon.assert.calledWith(myPieController.outcome, model[0], {
+        id: '1',
+        value: 'session'
+      }, {
+        mode: 'gather'
+      });
+    });
+
+    it('should return the result in the promise', () => {
+      outcomeResults.should.eql([{
+        id: '1',
+        value: 'outcome-response'
+      }]);
+    });
   });
 
-  it('should return the result in the promise', () => {
-    modelResults.should.eql([{ id: '1', value: 'model-response' }]);
+  describe('model', () => {
+
+    let modelResults;
+
+    beforeEach((done) => {
+      controller.model(['1'], [{
+          id: '1',
+          value: 'session'
+      }], {
+          mode: 'gather'
+        })
+        .then((results) => {
+          modelResults = results;
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should delegate calls to the underlying controllers', () => {
+      sinon.assert.calledWith(myPieController.model, model[0], {
+        id: '1',
+        value: 'session'
+      }, {
+        mode: 'gather'
+      });
+    });
+
+    it('should return the result in the promise', () => {
+      modelResults.should.eql([{
+        id: '1',
+        value: 'model-response'
+      }]);
+    });
   });
 });
