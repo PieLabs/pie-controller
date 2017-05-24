43688d17983fdf40eb625ee1b018fdf559340a03
Ed Eustace <ed.eustace@gmail.com>
2017-05-24 14:32:23 +0100

    feat(scoring): if score is null or undefined set weight to 0

--- test/scoring-test.js
+++ test/scoring-test.js
@@ -46,4 +46,21 @@ describe('scoring', () => {
       });
   });
 
+  it('sets weight to 0 for non scoreable outcomes', () => score(config, sessions, [{ id: '1' }])
+    .then(outcome => {
+      outcome.should.eql({
+        summary: {
+          max: 0, min: 0, percentage: 0
+        },
+        pies: [
+          { id: '1' }
+        ],
+        weights: [
+          { id: '1', weight: 0 }
+        ]
+      })
+    })
+
+  );
+
 });
