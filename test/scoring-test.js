750ffedcd72c0943495d500282e58f5db008d6cd
Ed Eustace <ed.eustace@gmail.com>
2017-05-24 14:36:32 +0100

    fix conflict

--- test/scoring-test.js
+++ test/scoring-test.js
@@ -28,22 +28,61 @@ describe('scoring', () => {
     ]
   });
 
-  it('returns a score', () => {
-
-    return score(config, sessions, outcomes)
-      .then(outcome => {
-        outcome.should.eql({
-          summary: {
-            max: 1, min: 0, percentage: 100
-          },
-          pies: [{
-            id: '1', score: 1
-          }],
-          weights: [
-            { id: '1', weight: 1 }
-          ]
-        });
+  it('returns a score', () => score(config, sessions, outcomes)
+    .then(outcome => {
+      outcome.should.eql({
+        summary: {
+          max: 1, min: 0, percentage: 100
+        },
+        pies: [{
+          id: '1', score: 1
+        }],
+        weights: [
+          { id: '1', weight: 1 }
+        ]
       });
-  });
+    })
+  );
 
+  it('sets weight to 0 for non scoreable outcomes', () => score(config, sessions, [{ id: '1' }])
+    .then(outcome => {
+      outcome.should.eql({
+        summary: {
+          max: 0, min: 0, percentage: 0
+        },
+        pies: [
+          { id: '1' }
+        ],
+        weights: [
+          { id: '1', weight: 0 }
+        ]
+      })
+    })
+  );
+
+  it('can work with scoreable and non scoreable', () => score({
+    models: [
+      { id: '1' },
+      { id: '2' }
+    ],
+    weights: [
+      { id: '2', weight: 10 }
+    ]
+  }
+    , sessions, [{ id: '1' }, { id: '2', score: 1 }])
+    .then(outcome => {
+      outcome.should.eql({
+        summary: {
+          max: 10, min: 0, percentage: 100
+        },
+        pies: [
+          { id: '1' }, { id: '2', score: 1 }
+        ],
+        weights: [
+          { id: '2', weight: 10 },
+          { id: '1', weight: 0 }
+        ]
+      })
+    })
+  );
 });
